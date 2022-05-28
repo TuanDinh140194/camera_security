eabcfe1ce28f4a966cdcd5117aa2dfeac717cf70
TuanDinh140194 <tnd@pdx.edu>
2022-05-28 11:57:27 -0700

    make it better

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -13,7 +13,6 @@ import Page404 from '../pages/Page404';
 import Page403 from '../pages/Page403';
 import { AuthContext } from '../context/auth';
 import Profile from '../pages/Profilepage';
-// import Setting from '../pages/Settingpage';
 import Requests from '../pages/Requests';
 import RoomPage from '../pages/Roompage';
 
@@ -23,15 +22,14 @@ class Navigation extends Component {
 			<AuthContext.Provider>
 				<HashRouter>
 					<Switch>
-						<Route exact path="/login" component={Login} />
-						<Route exact path='/homepage' component={Home} />
-						<Route exact path='/requests' component={Requests} />
-						<Route exact path='/profile' component={Profile} />
-						{/* <Route exact path='/setting' component={Setting} /> */}
-						<Route exact path='/room' component={RoomPage} />
-						<Route path="/404" component={Page404} />
-						<Route path="/403" component={Page403} />
-						<Route path='*' component={Login} />
+						<Route exact path={'/login'} component={Login} />
+						<Route exact path={'/homepage'} component={Home} />
+						<Route exact path={'/requests'} component={Requests} />
+						<Route exact path={'/profile'} component={Profile} />
+						<Route exact path={'/room'} component={RoomPage} />
+						<Route path={"/404"} component={Page404} />
+						<Route path={"/403"} component={Page403} />
+						<Route path={'*'} component={Login} />
 					</Switch>
 				</HashRouter>
 			</AuthContext.Provider>
